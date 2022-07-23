63794ce8fe081247f4afd9f6eea5241228b41665
Kirmiir <Kirmiir@outlook.com>
2022-07-23 14:55:54 +0300

    Initial Commit

---
+++ RazorTemplateEmptyNamespace/Program.cs
@@ -0,0 +1,56 @@
+﻿using RazorEngineCore;
+
+public class TestModelWithoutNamespace
+{
+    public string Name { get; set; }
+    public int[] Items { get; set; }
+}
+
+public class Program
+{
+	public static void Main()
+	{
+
+        Example1();
+        Example2();
+    }
+
+    private static void Example1()
+    {
+        IRazorEngine razorEngine = new RazorEngine();
+        string content = "Hello @Model.Name";
+        IRazorEngineCompiledTemplate<RazorEngineTemplateBase<TestModelWithoutNamespace>> template = razorEngine.Compile<RazorEngineTemplateBase<TestModelWithoutNamespace>>(content);
+
+        string result = template.Run(instance =>
+        {
+            instance.Model = new TestModelWithoutNamespace()
+            {
+                Name = "Hello",
+                Items = new[] { 3, 1, 2 }
+            };
+        });
+
+        Console.WriteLine(result);
+
+    }
+
+    private static void Example2()
+    {
+        IRazorEngine razorEngine = new RazorEngine();
+        string content = "Hello @Model.Name";
+
+        IRazorEngineCompiledTemplate<RazorEngineTemplateBase<RazorTemplateEmptyNamespace.TestModel.TestModelInnerClass>> template2 = razorEngine.Compile<RazorEngineTemplateBase<RazorTemplateEmptyNamespace.TestModel.TestModelInnerClass>>(content);
+
+        string result2 = template2.Run(instance =>
+        {
+            instance.Model = new RazorTemplateEmptyNamespace.TestModel.TestModelInnerClass()
+            {
+                Name = "Hello",
+                Items = new[] { 3, 1, 2 }
+            };
+        });
+
+        Console.WriteLine(result2);
+
+    }
+}
---
+++ RazorTemplateEmptyNamespace/TestModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorTemplateEmptyNamespace
+{
+    public  class TestModel
+    {
+        public class TestModelInnerClass
+        {
+            public string Name { get; set; }
+            public int[] Items { get; set; }
+        }
+    }
+}
